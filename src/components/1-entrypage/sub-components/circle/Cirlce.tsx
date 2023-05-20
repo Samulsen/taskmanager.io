3ade217f6878d93ad006b4aae2eec7eda426c970
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-20 22:48:51 +0200

    Added Rect, Circle, Safepoint before ref

---
+++ src/components/1-entrypage/sub-components/circle/Cirlce.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_circle.module.scss";
+
+//---------COMPONENT----------\
+
+const Cirlce = function () {
+  return <div></div>;
+};
+
+//---------EXPORTS------------\
+
+export default Cirlce;
---
+++ src/components/1-entrypage/sub-components/rectangle/Rectangle.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_rectangle.module.scss";
+
+//---------COMPONENT----------\
+
+const Rectangle = function () {
+  return <div></div>;
+};
+
+//---------EXPORTS------------\
+
+export default Rectangle;
