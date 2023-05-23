024de865082ce888657a531e913871198f85f48c
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-23 19:45:18 +0200

    added title as compo

--- src/components/0-independent/title/Title.tsx
+++ src/components/0-independent/title/Title.tsx
@@ -5,7 +5,13 @@ import classes from "./_Title.module.scss";
 //---------COMPONENT----------\
 
 const Title = function () {
-  return <div></div>;
+  return (
+    <div className={classes.body}>
+      <span className={classes.part_1}>taskmanager</span>
+      <span className={classes.part_2}>.</span>
+      <span className={classes.part_3}>io</span>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
--- src/components/1-entrypage/middle-area/MiddleArea.tsx
+++ src/components/1-entrypage/middle-area/MiddleArea.tsx
@@ -2,13 +2,16 @@
 
 import classes from "./_MiddleArea.module.scss";
 import Logo from "../../0-independent/logo/Logo";
+import Title from "../../0-independent/title/Title";
 
 //---------COMPONENT----------\
 
 const MiddleArea = function () {
   return (
     <div className={classes.middle}>
-      <div className={classes.title}>taskmanager.io</div>
+      <div className={classes.title}>
+        <Title />
+      </div>
       <div className={classes.subline}>Welcome, click somewhere to start!</div>
       <div className={classes.logo}>
         <Logo animation="entrypage"></Logo>
