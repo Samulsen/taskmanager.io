d4d026c505616bade47f560813c7a2a8472096e8
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-23 19:31:36 +0200

    title setup init

---
+++ src/components/0-independent/title/Title.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_Title.module.scss";
+
+//---------COMPONENT----------\
+
+const Title = function () {
+  return <div></div>;
+};
+
+//---------EXPORTS------------\
+
+export default Title;
