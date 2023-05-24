631876071451574053c1cac00df38b8804ee3fa9
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-24 14:32:35 +0200

    toggler symbols added without logic

--- src/components/2-loginpage/passwordToggler/PasswordToggler.tsx
+++ src/components/2-loginpage/passwordToggler/PasswordToggler.tsx
@@ -1,7 +1,17 @@
+//---------IMPORTS------------\
+
+import showSign from "./show.svg";
+import unshowSign from "./unshow.svg";
+
 //---------COMPONENT----------\
 
 const PasswordToggler: React.FC<{ drilledClass: string }> = function (props) {
-  return <div className={props.drilledClass}></div>;
+  return (
+    <div className={props.drilledClass}>
+      <img src={showSign} alt="showSign" />
+      {/* <img src={unshowSign} alt="unshowSign" /> */}
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
