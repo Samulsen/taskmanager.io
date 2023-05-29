ebbd345b9dcbfa71c4528781d40e3078853df6f1
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-29 10:43:22 +0200

    firebase logic added to auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -11,7 +11,12 @@ import {
 } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
-import { User } from "firebase/auth";
+import { User, UserCredential } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 //__i-helper_________
 
@@ -20,19 +25,44 @@ import logCol from "../util/logColor";
 
 //---------MAIN---------------\
 
+//SECTION______________________: Type definitions
+
 const state = {
   //__NOTE: by default not evaluated because of async op of firebase auth, when returning => either true or false, private and public subscribed!
   cold: "UNEVALUATED",
   loggedIn: true,
   notLoggedIn: false,
 };
-
 type userStates = null | User;
 type authStates = string | boolean;
+type firebaseAuthMethod = (
+  email: string,
+  password: string
+) => Promise<UserCredential>;
 interface ContextValueType {
   userObject: userStates;
   authState: authStates;
+  loggin: firebaseAuthMethod;
+  register: firebaseAuthMethod;
+  loggout: () => Promise<void>;
 }
+
+//SECTION______________________: Firebase functions
+
+const loggin = function (email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const register = function (email: string, password: string) {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const loggout = function () {
+  return signOut(auth);
+};
+
+//SECTION______________________: Context Object
+
 const AuthContextLocal = createContext<ContextValueType | null>(null);
 
 //---------COMPONENT----------\
@@ -63,6 +93,9 @@ const AuthContextProvider: React.FC<{ children: ReactNode }> = function ({
   const AuthContextValues: ContextValueType = {
     userObject,
     authState,
+    loggin,
+    register,
+    loggout,
   };
 
   return (
