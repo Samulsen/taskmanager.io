7dc01e132a512ef0266e7590212e6b8eab0b7954
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-30 10:41:33 +0200

    RegisterForm setup

---
+++ src/components/3-registerpage/registerForm/RegisterForm.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_RegisterForm.module.scss";
+
+//---------COMPONENT----------\
+
+const RegisterForm = function () {
+  return <div className={classes.body}></div>;
+};
+
+//---------EXPORTS------------\
+
+export default RegisterForm;
---
+++ src/components/3-registerpage/registerForm/checkedInput/CheckedInput.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_CheckedInput.module.scss";
+
+//---------COMPONENT----------\
+
+const CheckedInput = function () {
+  return <div></div>;
+};
+
+//---------EXPORTS------------\
+
+export default CheckedInput;
