8f219e1116edd97fbe0b672a8b7dcd04214affda
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-30 18:03:04 +0200

    form with two inputs

--- src/components/3-registerpage/registerForm/RegisterForm.tsx
+++ src/components/3-registerpage/registerForm/RegisterForm.tsx
@@ -2,6 +2,7 @@
 
 import classes from "./_RegisterForm.module.scss";
 import CheckedInput from "./checkedInput/CheckedInput";
+import { useRef } from "react";
 
 //---------COMPONENT----------\
 
@@ -9,11 +10,18 @@ const RegisterForm = function () {
   return (
     <div className={classes.body}>
       <CheckedInput
-        key="inpName"
+        key="inpNameFirst"
         type="text"
         individualClass={classes.firstName}
         placeholder="First Name"
         name="input-firstName"
+      />
+      <CheckedInput
+        key="inpNameLast"
+        type="text"
+        individualClass={classes.lastName}
+        placeholder="Last Name"
+        name="input-lastName"
       />
     </div>
   );
