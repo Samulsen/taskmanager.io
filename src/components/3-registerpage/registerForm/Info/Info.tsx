064466d61faca1e83eb7bbb05386ce16ea57a6c3
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-30 19:58:50 +0200

    added info component

---
+++ src/components/3-registerpage/registerForm/Info/Info.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_Info.module.scss";
+
+//---------COMPONENT----------\
+
+const Info = function () {
+  return <div></div>;
+};
+
+//---------EXPORTS------------\
+
+export default Info;
