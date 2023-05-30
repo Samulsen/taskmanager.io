4166c92ecca2c1fc30c0fc7b016ef4c2df6686ce
Samuil Berenzon <samus.dev.space@gmail.com>
2023-05-30 11:26:39 +0200

    inital config of input v1

--- src/components/3-registerpage/registerForm/checkedInput/CheckedInput.tsx
+++ src/components/3-registerpage/registerForm/checkedInput/CheckedInput.tsx
@@ -1,12 +1,34 @@
 //---------IMPORTS------------\
 
 import classes from "./_CheckedInput.module.scss";
+import { forwardRef, useState } from "react";
+
+//---------MAIN---------------\
+
+interface props {
+  type: string;
+  individualClass: string;
+  placeholder: string;
+  name: string;
+}
+type ref = HTMLInputElement;
 
 //---------COMPONENT----------\
 
-const CheckedInput = function () {
-  return <div></div>;
-};
+const CheckedInput = forwardRef<ref, props>((props, ref) => {
+  const [value, setValue] = useState();
+
+  return (
+    <input
+      value={value}
+      ref={ref}
+      name={props.name}
+      type={props.type}
+      className={`${classes.body} ${props.individualClass}`}
+      placeholder={props.placeholder}
+    />
+  );
+});
 
 //---------EXPORTS------------\
 
