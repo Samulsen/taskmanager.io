ac2d1a90a1fbd279c910c5f818ddec566bffcc91
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-05 00:36:47 +0200

    half done debuglogger

--- src/util/debugLoggerData.ts
+++ src/util/debugLoggerData.ts
@@ -5,9 +5,31 @@ import { appMetaData } from "../context/DataContext";
 //---------MAIN---------------\
 
 const debugLoggerData = function (appMetaData: appMetaData | string) {
-  console.log(`
-
-`);
+  console.log(
+    `
+%cDataContext evaluates AppMetaData...
+%cCurrent State: %c${appMetaData === "cold" ? "cold" : "accessed"}
+%c${
+      appMetaData === "cold"
+        ? "Waiting for onSnapshot listener initiation!"
+        : "Current AppMetaData Values:"
+    }
+%c${
+      appMetaData === "cold"
+        ? "continuing..."
+        : `Name: ${(appMetaData as appMetaData).firstName} ${
+            (appMetaData as appMetaData).lastName
+          }
+        
+        `
+    }
+`,
+    "font-weight: bold; color:rgb(21, 255, 181)",
+    "color:white",
+    `${appMetaData === "cold" ? "color:orange" : "color: yellowgreen"}`,
+    `${appMetaData === "cold" ? "color:rgb(255, 224, 146)" : "color: green"}`,
+    "color:white"
+  );
 };
 
 //---------EXPORTS------------\
