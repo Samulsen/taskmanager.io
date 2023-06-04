741c76167b0281922fad40f4653cf333fb1be45a
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-04 17:59:10 +0200

    primitive boilerplate for DataContext

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,14 +1,37 @@
 //---------IMPORTS------------\
 
 import { db } from "../firebase";
-import { createContext, FC, ReactNode } from "react";
+import { createContext, FC, ReactNode, useContext } from "react";
+
+//---------MAIN---------------\
+
+//SECTION______________________: Type definitions
+
+interface ContextValueType {}
+
+//SECTION______________________: Firebase References
+
+//SECTION______________________: Context Object
+
+const DataContextLocal = createContext<ContextValueType | null>(null);
 
 //---------COMPONENT----------\
 
-const DataContext = function () {
-  return <div></div>;
+const DataContextProvider: FC<{ children: ReactNode }> = function ({
+  children,
+}) {
+  const DataContextValues: ContextValueType = {};
+
+  return (
+    <DataContextLocal.Provider value={DataContextValues}>
+      {children}
+    </DataContextLocal.Provider>
+  );
 };
 
 //---------EXPORTS------------\
 
-export default DataContext;
+export default DataContextProvider;
+export const DataContext = function () {
+  return useContext(DataContextLocal);
+};
