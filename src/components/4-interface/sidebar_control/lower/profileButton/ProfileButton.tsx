7bb7f5f9de35a3ffe76d212b47ba92ee97de88d7
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-06 23:45:21 +0200

    added lower menu html struc

--- src/components/4-interface/sidebar_control/lower/profileButton/ProfileButton.tsx
+++ src/components/4-interface/sidebar_control/lower/profileButton/ProfileButton.tsx
@@ -5,7 +5,7 @@ import classes from "./_ProfileButton.module.scss";
 //---------COMPONENT----------\
 
 const ProfileButton = function () {
-  return <div className={classes.body}></div>;
+  return <div className={classes.body}>SB</div>;
 };
 
 //---------EXPORTS------------\
