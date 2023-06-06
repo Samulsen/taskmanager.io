d7b04e459dee4bcb213292840a28c29b3b7702c7
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-06 15:23:02 +0200

    controll sidebar with scrollbar custom and overlow

--- src/components/4-interface/sidebar_control/SidebarControl.tsx
+++ src/components/4-interface/sidebar_control/SidebarControl.tsx
@@ -1,11 +1,20 @@
 //---------IMPORTS------------\
 
+import Title from "../../0-independent/title/Title";
 import classes from "./_SidebarControl.module.scss";
 
 //---------COMPONENT----------\
 
 const SidebarControl = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.menu_upper}></div>
+      <div className={classes.header}>
+        <Title individualClass={classes.transfomer} />
+      </div>
+      <div className={classes.menu_lower}></div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
