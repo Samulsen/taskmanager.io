4fd0f91766518590cb8936e8b9941a9102c7469d
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 00:02:30 +0200

    pre bug fix safe, handling async data load

--- src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutButton.tsx
+++ src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutButton.tsx
@@ -1,14 +1,17 @@
 //---------IMPORTS------------\
 
+import { AuthContext } from "../../../../../context/AuthContext";
 import classes from "./_LoggoutButton.module.scss";
 import icon from "./loggoutIcon.svg";
 
 //---------COMPONENT----------\
 
 const LoggoutButton = function () {
+  const loggout = AuthContext()!.loggout;
+
   return (
     <div className={classes.body}>
-      <img src={icon} alt="loggoutButtonIcon" />
+      <img src={icon} alt="loggoutButtonIcon" onClick={loggout} />
     </div>
   );
 };
