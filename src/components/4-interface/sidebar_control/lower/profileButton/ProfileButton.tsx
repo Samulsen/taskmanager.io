c827abed0e758f9d482133fbb7699eaf04384edb
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-06 23:49:40 +0200

    added dynamic usermeta in profile button

--- src/components/4-interface/sidebar_control/lower/profileButton/ProfileButton.tsx
+++ src/components/4-interface/sidebar_control/lower/profileButton/ProfileButton.tsx
@@ -1,11 +1,19 @@
 //---------IMPORTS------------\
 
 import classes from "./_ProfileButton.module.scss";
+import { DataContext, appMetaData } from "../../../../../context/DataContext";
 
 //---------COMPONENT----------\
 
 const ProfileButton = function () {
-  return <div className={classes.body}>SB</div>;
+  const UserMetaData = DataContext()!.appMetaData as appMetaData;
+  const Logic = {
+    evaluateUserShort() {
+      const userInitials = UserMetaData.firstName[0] + UserMetaData.lastName[0];
+      return userInitials;
+    },
+  };
+  return <div className={classes.body}>{Logic.evaluateUserShort()}</div>;
 };
 
 //---------EXPORTS------------\
