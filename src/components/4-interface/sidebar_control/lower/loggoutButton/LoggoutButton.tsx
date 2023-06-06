f9d822656b4b2f186aaf16d7b971b2fdc6801d1d
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-06 23:40:46 +0200

    loggout button added

--- src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutButton.tsx
+++ src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutButton.tsx
@@ -1,11 +1,16 @@
 //---------IMPORTS------------\
 
 import classes from "./_LoggoutButton.module.scss";
+import icon from "./loggoutIcon.svg";
 
 //---------COMPONENT----------\
 
 const LoggoutButton = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <img src={icon} alt="loggoutButtonIcon" />
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
