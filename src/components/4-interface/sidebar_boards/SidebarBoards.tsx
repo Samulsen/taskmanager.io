0b8221b7d2c0d2861b128bf6cb5d9c509d9f966e
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 21:19:53 +0200

    added grid to boardbar

--- src/components/4-interface/sidebar_boards/SidebarBoards.tsx
+++ src/components/4-interface/sidebar_boards/SidebarBoards.tsx
@@ -1,11 +1,18 @@
 //---------IMPORTS------------\
 
 import classes from "./_SidebarBoards.module.scss";
+import Line from "../../0-independent/line/Line";
 
 //---------COMPONENT----------\
 
 const SidebarBoards = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.menu}></div>
+      <div className={classes.list}></div>
+      <Line />
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
