a54be8b41417a6f48148f7b238bddefd22779df7
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 21:40:54 +0200

    added svgs to menu

--- src/components/4-interface/sidebar_boards/boardBarMenu/BoardBarMenu.tsx
+++ src/components/4-interface/sidebar_boards/boardBarMenu/BoardBarMenu.tsx
@@ -1,11 +1,20 @@
 //---------IMPORTS------------\
 
 import classes from "./_BoardBarMenu.module.scss";
+import { Link } from "react-router-dom";
 
 //---------COMPONENT----------\
 
 const BoardBarMenu = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.title}></div>
+      <Link className={classes.linkTotal} to={""}>
+        See all tasks
+      </Link>
+      <div className={classes.newBoard}></div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
