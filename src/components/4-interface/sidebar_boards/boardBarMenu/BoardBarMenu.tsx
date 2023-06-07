1a338356dc84a1a4b2c7b58e2e5b2ca02bd6af36
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 21:57:30 +0200

    pre flex on all childrens

--- src/components/4-interface/sidebar_boards/boardBarMenu/BoardBarMenu.tsx
+++ src/components/4-interface/sidebar_boards/boardBarMenu/BoardBarMenu.tsx
@@ -2,17 +2,25 @@
 
 import classes from "./_BoardBarMenu.module.scss";
 import { Link } from "react-router-dom";
+import Line from "../../../0-independent/line/Line";
+import iconAllTasks from "./allTasks.svg";
+import iconNewBoard from "./newBoard.svg";
 
 //---------COMPONENT----------\
 
 const BoardBarMenu = function () {
   return (
     <div className={classes.body}>
-      <div className={classes.title}></div>
+      <div className={classes.title}>Workspace</div>
       <Link className={classes.linkTotal} to={""}>
+        <img className={classes.icon} src={iconAllTasks} alt="allTaskBtton" />
         See all tasks
       </Link>
-      <div className={classes.newBoard}></div>
+      <div className={classes.newBoard}>
+        <img className={classes.icon} src={iconNewBoard} alt="newBoardBtton" />
+        Add new Board
+      </div>
+      <Line />
     </div>
   );
 };
