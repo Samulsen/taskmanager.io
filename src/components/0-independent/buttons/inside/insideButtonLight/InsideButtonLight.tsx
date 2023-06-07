0c52bde3c84883b80e3779fc81435a3c3c5d963f
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 15:19:15 +0200

    first version of logout opo

--- src/components/0-independent/buttons/inside/insideButtonLight/InsideButtonLight.tsx
+++ src/components/0-independent/buttons/inside/insideButtonLight/InsideButtonLight.tsx
@@ -1,11 +1,31 @@
 //---------IMPORTS------------\
 
 import classes from "./_InsideButtonLight.module.scss";
+import { FC } from "react";
+
+//---------MAIN---------------\
+
+interface props {
+  // individualClass: string;
+  clickMethod: () => void;
+  name: string;
+  background: string;
+}
 
 //---------COMPONENT----------\
 
-const InsideButtonLight = function () {
-  return <div className={classes.body}></div>;
+const InsideButtonLight: FC<props> = function (props) {
+  const Logic = {
+    evaluateColor() {
+      return props.background === "neutral" ? classes.neutral : classes.reddish;
+    },
+  };
+
+  return (
+    <div className={`${classes.body} ${Logic.evaluateColor()}`}>
+      {props.name}
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
--- src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutConfirmPop/LoggoutConfirmPop.tsx
+++ src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutConfirmPop/LoggoutConfirmPop.tsx
@@ -1,5 +1,6 @@
 //---------IMPORTS------------\
 
+import InsideButtonLight from "../../../../../0-independent/buttons/inside/insideButtonLight/InsideButtonLight";
 import classes from "./_LoggoutConfirmPop.module.scss";
 
 //---------COMPONENT----------\
@@ -9,6 +10,18 @@ const LoggoutConfirmPop = function () {
     <>
       <div className={classes.body}>
         <div className={classes.message}>Do you want to log out?</div>
+        <InsideButtonLight
+          key="buttonCancelLoggout"
+          name="Cancel"
+          background="neutral"
+          clickMethod={() => ""}
+        />
+        <InsideButtonLight
+          key="buttonLoggoutLogout"
+          name="Logout"
+          background="reddish"
+          clickMethod={() => ""}
+        />
       </div>
       <div className={classes.pointer}></div>
     </>
