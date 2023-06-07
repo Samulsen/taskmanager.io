e43957bcab58a18ebc5cc5e3f1fbc4c8c38ddce9
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-07 11:51:37 +0200

    added LightButton and LoggoutConfirm compo base

---
+++ src/components/0-independent/buttons/inside/insideButtonLight/InsideButtonLight.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_InsideButtonLight.module.scss";
+
+//---------COMPONENT----------\
+
+const InsideButtonLight = function () {
+  return <div className={classes.body}></div>;
+};
+
+//---------EXPORTS------------\
+
+export default InsideButtonLight;
---
+++ src/components/4-interface/sidebar_control/lower/loggoutButton/LoggoutConfirmPop/LoggoutConfirmPop.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_LoggoutConfirmPop.module.scss";
+
+//---------COMPONENT----------\
+
+const LoggoutConfirmPop = function () {
+  return <div className={classes.body}></div>;
+};
+
+//---------EXPORTS------------\
+
+export default LoggoutConfirmPop;
