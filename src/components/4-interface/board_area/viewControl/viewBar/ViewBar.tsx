b1877edb411c6e69be7abbdf252dc854a01f475e
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-08 19:05:32 +0200

    added single compo to bar with seperators

--- src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
+++ src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
@@ -1,16 +1,30 @@
 //---------IMPORTS------------\
 
 import classes from "./_ViewBar.module.scss";
+import EffectFilter from "./effectFilter/EffectFilter";
+import EffectSort from "./effectSort/EffectSort";
 import SepBig from "./seperator/sepBig/SepBig";
 import SepSmall from "./seperator/sepSmall/SepSmall";
+import ViewHome from "./viewHome/ViewHome";
+import ViewProgress from "./viewProgress/ViewProgress";
+import ViewUntouched from "./viewUntouched/ViewUntouched";
 
 //---------COMPONENT----------\
 
 const ViewBar = function () {
   return (
     <div className={classes.body}>
-      <SepBig />
+      <ViewHome />
       <SepSmall />
+      <ViewHome />
+      <SepSmall />
+      <ViewProgress />
+      <SepSmall />
+      <ViewUntouched />
+      <SepBig />
+      <EffectFilter />
+      <SepSmall />
+      <EffectSort />
     </div>
   );
 };
