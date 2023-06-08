8abfc3f87f19ae799edbff1aa9eb989c9963e441
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-08 20:22:30 +0200

    added selection style logic

--- src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
+++ src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
@@ -1,5 +1,7 @@
 //---------IMPORTS------------\
 
+import { useState } from "react";
+
 //__i-style__________
 import classes from "./_ViewBar.module.scss";
 import homeIcon from "./svgs/homeIcon.svg";
@@ -17,15 +19,37 @@ import SepSmall from "./seperator/sepSmall/SepSmall";
 //---------COMPONENT----------\
 
 const ViewBar = function () {
+  const [selectionState, setSelectionState] = useState("Home");
+
   return (
     <div className={classes.body}>
-      <ViewTemplate icon={homeIcon} effect="Home" />
+      <ViewTemplate
+        icon={homeIcon}
+        effect="Home"
+        selectionState={selectionState}
+        setSelection={setSelectionState}
+      />
       <SepSmall />
-      <ViewTemplate icon={doneIcon} effect="Done" />
+      <ViewTemplate
+        icon={doneIcon}
+        effect="Done"
+        selectionState={selectionState}
+        setSelection={setSelectionState}
+      />
       <SepSmall />
-      <ViewTemplate icon={progressIcon} effect="In Progress" />
+      <ViewTemplate
+        icon={progressIcon}
+        effect="In Progress"
+        selectionState={selectionState}
+        setSelection={setSelectionState}
+      />
       <SepSmall />
-      <ViewTemplate icon={untouchedIcon} effect="Untouched" />
+      <ViewTemplate
+        icon={untouchedIcon}
+        effect="Untouched"
+        selectionState={selectionState}
+        setSelection={setSelectionState}
+      />
       <SepBig />
       <EffectFilter />
       <SepSmall />
--- src/components/4-interface/board_area/viewControl/viewBar/viewTemplate/ViewTemplate.tsx
+++ src/components/4-interface/board_area/viewControl/viewBar/viewTemplate/ViewTemplate.tsx
@@ -1,28 +1,53 @@
 //---------IMPORTS------------\
 
 import classes from "./_ViewTemplate.module.scss";
-import { FC } from "react";
+import { FC, Dispatch, SetStateAction } from "react";
 
 //---------MAIN---------------\
 
 interface props {
   effect: string;
   icon: string;
+  selectionState: string;
+  setSelection: Dispatch<SetStateAction<string>>;
 }
 
 //---------COMPONENT----------\
 
-const ViewTemplate: FC<props> = function ({ effect, icon }) {
+const ViewTemplate: FC<props> = function ({
+  effect,
+  icon,
+  selectionState,
+  setSelection,
+}) {
   const Logic = {
+    setSelfSelection() {
+      setSelection(effect);
+    },
     evaluateStyle() {
-      return true
+      let mySelectionState;
+
+      if (selectionState === "Home" && selectionState === effect) {
+        mySelectionState = true;
+      }
+      if (selectionState === "Done" && selectionState === effect) {
+        mySelectionState = true;
+      }
+      if (selectionState === "In Progress" && selectionState === effect) {
+        mySelectionState = true;
+      }
+      if (selectionState === "Untouched" && selectionState === effect) {
+        mySelectionState = true;
+      }
+
+      return mySelectionState
         ? `${classes.body} ${classes.selected}`
         : `${classes.body} ${classes.unselected}`;
     },
   };
 
   return (
-    <div className={Logic.evaluateStyle()}>
+    <div onClick={Logic.setSelfSelection} className={Logic.evaluateStyle()}>
       <img className={classes.icon} src={icon} alt={`viewIcon${effect}`} />
       View - {effect}
     </div>
