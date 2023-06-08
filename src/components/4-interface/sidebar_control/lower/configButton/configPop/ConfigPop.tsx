5d46e271668a0a198fc7c63ca40362813473df0d
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-08 23:44:45 +0200

    added flex struc and items

--- src/components/4-interface/sidebar_control/lower/configButton/configPop/ConfigPop.tsx
+++ src/components/4-interface/sidebar_control/lower/configButton/configPop/ConfigPop.tsx
@@ -5,7 +5,17 @@ import classes from "./_ConfigPop.module.scss";
 const ConfigPop = function () {
   return (
     <>
-      <div className={classes.body}></div>
+      <div className={classes.body}>
+        <div className={classes.header}>Settings</div>
+        <div className={classes.settings}>
+          <div className={classes.detail}>
+            Delete Task when Status is <span>"Done!"</span>?
+          </div>
+          <div className={classes.arrow}></div>
+          <div className={classes.tickbox}></div>
+        </div>
+        <div className={classes.confirm}>Save & Back</div>
+      </div>
       <div className={classes.pointer}></div>
     </>
   );
