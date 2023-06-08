5c9bd69523d7a0143670f9d2ce77867ca7a4d09b
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-08 14:39:53 +0200

    added view controll compo

---
+++ src/components/4-interface/board_area/viewControll/ViewControl.tsx
@@ -0,0 +1,13 @@
+//---------IMPORTS------------\
+
+import classes from "./_ViewControl.module.scss";
+
+//---------COMPONENT----------\
+
+const ViewControl = function () {
+  return <div className={classes.body}></div>;
+};
+
+//---------EXPORTS------------\
+
+export default ViewControl;
