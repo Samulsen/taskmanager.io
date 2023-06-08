e9f13920bfc54113fc2a15948a957b0e295fc05a
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-08 19:16:04 +0200

    added template for views

---
+++ src/components/4-interface/board_area/viewControl/viewBar/viewTemplate/ViewTemplate.tsx
@@ -0,0 +1,10 @@
+//---------IMPORTS------------\
+import classes from "./_ViewTemplate.module.scss";
+
+//---------COMPONENT----------\
+const ViewTemplate = function () {
+  return <div className={classes.body}></div>;
+};
+
+//---------EXPORTS------------\
+export default ViewTemplate;
