f7ed9bec6936bf4d1db193d2577155157fe2c9f4
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-11 23:39:34 +0200

    added confirm and abort sass struc

--- src/components/4-interface/sidebar_boards/boardBarList/boardItem/ItemMenu/ItemListMenu.tsx
+++ src/components/4-interface/sidebar_boards/boardBarList/boardItem/ItemMenu/ItemListMenu.tsx
@@ -1,14 +1,59 @@
 //---------IMPORTS------------\
 
-import { Dispatch, FC, SetStateAction } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 import classes from "./_ItemListMenu.module.scss";
-import deleteIcon from "./delete.svg";
-import renameIcon from "./rename.svg";
+import deleteIcon from "./svgs/delete.svg";
+import renameIcon from "./svgs/rename.svg";
+import confirmIcon from "./svgs/confirm.svg";
+import abortIcon from "./svgs/abort.svg";
 import useClickOutside from "../../../../../../hooks/useClickOutside";
 
 //---------COMPONENT----------\
 const ItemListMenu: FC<{ setMenuState: Dispatch<SetStateAction<boolean>> }> =
   function ({ setMenuState }) {
+    const [requestDeletion, setRegquestDeletion] = useState(false);
+
+    const Logic = {
+      UI: {
+        requestDeletion() {
+          setRegquestDeletion(true);
+        },
+        evaluateDeletionRequest() {
+          if (requestDeletion) {
+            return (
+              <div className={classes.option}>
+                <div className={classes.confirm}>
+                  <img src={confirmIcon} alt="confirmDeletion" />
+                </div>
+                ?
+                <div className={classes.abort}>
+                  <img src={abortIcon} alt="abortDeletion" />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div
+                className={classes.option}
+                onClick={Logic.UI.requestDeletion}
+              >
+                <img
+                  className={classes.icon}
+                  src={deleteIcon}
+                  alt="deleteIconOnItem"
+                />
+                Delete
+              </div>
+            );
+          }
+        },
+      },
+      Data: {
+        abortDeletion() {},
+        confirmDeletion() {},
+      },
+    };
+
     return (
       <>
         <div ref={useClickOutside(setMenuState)} className={classes.body}>
@@ -20,14 +65,8 @@ const ItemListMenu: FC<{ setMenuState: Dispatch<SetStateAction<boolean>> }> =
             />
             Rename
           </div>
-          <div className={classes.option}>
-            <img
-              className={classes.icon}
-              src={deleteIcon}
-              alt="deleteIconOnItem"
-            />
-            Delete
-          </div>
+
+          {Logic.UI.evaluateDeletionRequest()}
         </div>
         <div className={classes.pointer}></div>
       </>
