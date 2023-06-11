7b2c4e7661849c7dff92325022dda5ed8b05f2cf
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-11 20:34:34 +0200

    board list renders corretly

--- src/components/4-interface/sidebar_boards/boardBarList/BoardBarList.tsx
+++ src/components/4-interface/sidebar_boards/boardBarList/BoardBarList.tsx
@@ -2,33 +2,38 @@
 
 import classes from "./_BoardBarList.module.scss";
 import BoardItem from "./boardItem/BoardItem";
-import { DataContext, appMetaData } from "../../../../context/DataContext";
+import { BoardlistContext } from "../../../../context/BoardlistContext";
 
 //---------COMPONENT----------\
 
 const BoardBarList = function () {
-  // const { boardNames: boardListArray } = DataContext()
-  //   ?.appMetaData as appMetaData;
+  const { state, boardNames } = BoardlistContext();
 
-  // const Logic = {
-  //   UI: {
-  //     renderList() {
-  //       return boardListArray
-  //         .filter(([boardId]) => boardId !== "initialField")
-  //         .sort((a, b) => b[1].timestamp.seconds - a[1].timestamp.seconds)
-  //         .map(([boardId, { name }]) => (
-  //           <BoardItem
-  //             key={boardId}
-  //             boardId={boardId}
-  //             currentBoardName={name}
-  //           />
-  //         ));
-  //     },
-  //   },
-  // };
+  const Logic = {
+    evaluateRenderState() {
+      if (state === "cold") {
+        return <></>;
+      } else {
+        return this.UI.renderList();
+      }
+    },
+    UI: {
+      renderList() {
+        return boardNames
+          .filter(([boardId]) => boardId !== "initialField")
+          .sort((a, b) => b[1].timestamp.seconds - a[1].timestamp.seconds)
+          .map(([boardId, { name }]) => (
+            <BoardItem
+              key={boardId}
+              boardId={boardId}
+              currentBoardName={name}
+            />
+          ));
+      },
+    },
+  };
 
-  // return <div className={classes.body}>{Logic.UI.renderList()}</div>;
-  return <div className="div"></div>;
+  return <div className={classes.body}>{Logic.evaluateRenderState()}</div>;
 };
 
 //---------EXPORTS------------\
--- src/context/BoardlistContext.tsx
+++ src/context/BoardlistContext.tsx
@@ -1,23 +1,91 @@
 //---------IMPORTS------------\
 
-import { createContext, FC, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
+import { AuthContext } from "./AuthContext";
+import { DocumentData, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
 
 //---------MAIN---------------\
 
-interface ContextValueTypeBoardlist {}
+type BoardMetaData = {
+  name: string;
+  timestamp: { seconds: number; nanoseconds: number };
+};
+
+interface ContextValueTypeBoardlist {
+  state: string;
+  boardNames: [string, BoardMetaData][];
+}
+
+interface QueryData {
+  boardNames: {
+    [id: string]: BoardMetaData;
+  };
+}
+
+//SECTION______________________: InitData
+
+const coldData: ContextValueTypeBoardlist = {
+  state: "cold",
+  boardNames: [],
+};
 
 //SECTION______________________: Context objects
 
-const BoardlistContextLocal = createContext<ContextValueTypeBoardlist | null>(
-  null
-);
+const BoardlistContextLocal =
+  createContext<ContextValueTypeBoardlist>(coldData);
 
 const BoardlistContextProvider: FC<{ children: ReactNode }> = function ({
   children,
 }) {
   //__c-hooks________
+
+  const uid = AuthContext()?.userObject?.uid;
+  const [boardNamesList, updateBoardNamesList] = useState(coldData);
+
   //__c-logic________
-  const BoardlistContextValues: ContextValueTypeBoardlist = {};
+
+  const Logic = {
+    Data: {
+      tempData: {} as ContextValueTypeBoardlist,
+      deconstruct(boardNamesSnapshot: DocumentData) {
+        this.tempData = {
+          state: "warm",
+          boardNames: Object.entries(
+            boardNamesSnapshot.data()!.boardNames as QueryData
+          ) as [string, BoardMetaData][],
+        };
+        return Logic.Data;
+      },
+      merge() {
+        updateBoardNamesList(this.tempData);
+      },
+    },
+  };
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(db, `MainUserDataPool_${uid}`, "UserBoards"),
+      (boardNamesSnapshot) => {
+        if (!boardNamesSnapshot.metadata.hasPendingWrites) {
+          Logic.Data.deconstruct(boardNamesSnapshot).merge();
+        }
+      }
+    );
+
+    return unsub;
+  }, []);
+
+  const BoardlistContextValues: ContextValueTypeBoardlist = {
+    ...boardNamesList,
+  };
 
   return (
     <BoardlistContextLocal.Provider value={BoardlistContextValues}>
