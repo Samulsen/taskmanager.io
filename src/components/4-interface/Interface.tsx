9ab1cbe6791484353426d5aaff3965e5a55dac64
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-11 18:48:04 +0200

    removed imports on interface etc.

--- src/components/4-interface/Interface.tsx
+++ src/components/4-interface/Interface.tsx
@@ -2,58 +2,35 @@
 
 import { Outlet } from "react-router-dom";
 import classes from "./_Interface.module.scss";
-//__i-components_____
-import { DataContext, appMetaData } from "../../context/DataContext";
+//__i-context________
 import MetaDataContextProvider from "../../context/MetadataContext";
 import BoardContextProvider from "../../context/BoardContext";
+import ConfigContextProvider from "../../context/ConfigContext";
+import WorkspaceContextProvider from "../../context/WorkspaceContext";
+//__i-components_____
 import BoardArea from "./board_area/BoardArea";
 import SidebarBoards from "./sidebar_boards/SidebarBoards";
 import SidebarControl from "./sidebar_control/SidebarControl";
-import Anchor from "../0-independent/anchor/Anchor";
-import Loading from "../0-auth/loader/Loading";
-import ConfigContextProvider from "../../context/ConfigContext";
-import WorkspaceContextProvider from "../../context/WorkspaceContext";
 
 //---------COMPONENT----------\
 const Interface = function () {
-  //__c-hooks________
-
-  // const userMetaData = DataContext()!.appMetaData;
-
-  const Logic = {
-    evaluateConnectionMainDataPool() {
-      // if (userMetaData === "cold") {
-      if (false) {
-        return (
-          <Anchor>
-            <Loading />
-          </Anchor>
-        );
-      }
-      // if (typeof (userMetaData as appMetaData).firstName === "string") {
-      if (true) {
-        return (
-          <div className={classes.body}>
-            <MetaDataContextProvider>
-              <ConfigContextProvider>
-                <SidebarControl />
-              </ConfigContextProvider>
-            </MetaDataContextProvider>
-            <BoardContextProvider>
-              <WorkspaceContextProvider>
-                <SidebarBoards />
-              </WorkspaceContextProvider>
-              <BoardArea>
-                <Outlet />
-              </BoardArea>
-            </BoardContextProvider>
-          </div>
-        );
-      }
-    },
-  };
-
-  return <>{Logic.evaluateConnectionMainDataPool()}</>;
+  return (
+    <div className={classes.body}>
+      <MetaDataContextProvider>
+        <ConfigContextProvider>
+          <SidebarControl />
+        </ConfigContextProvider>
+      </MetaDataContextProvider>
+      <BoardContextProvider>
+        <WorkspaceContextProvider>
+          <SidebarBoards />
+        </WorkspaceContextProvider>
+        <BoardArea>
+          <Outlet />
+        </BoardArea>
+      </BoardContextProvider>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
