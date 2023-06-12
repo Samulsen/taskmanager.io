31390649a3d4ddd529e2d66e5c43056f0883095e
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-12 14:01:09 +0200

    loads correct ui on search param

--- src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
+++ src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
@@ -1,10 +1,20 @@
 //---------IMPORTS------------\
 
 import classes from "./_Dynamic.module.scss";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { BoardContext } from "../../../../../context/BoardContext";
 
 //---------COMPONENT----------\
 
 const Dynamic = function () {
+  const { boardID } = useParams();
+  const { boardControl } = BoardContext()!;
+
+  useEffect(() => {
+    boardControl.setState(boardID as string);
+  }, []);
+
   return <div className={classes.body}></div>;
 };
 
