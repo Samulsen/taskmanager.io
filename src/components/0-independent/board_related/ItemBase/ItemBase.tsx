bcaa8aa86b69a5b59b96840e15b29f508eec238e
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 20:24:12 +0200

    added box and grouptline to item

--- src/components/0-independent/board_related/ItemBase/ItemBase.tsx
+++ src/components/0-independent/board_related/ItemBase/ItemBase.tsx
@@ -5,7 +5,12 @@ import classes from "./_ItemBase.module.scss";
 const ItemBase = function () {
   return (
     <div className={classes.body}>
-      <div className={classes.main}></div>
+      <div className={classes.grouplineMid}></div>
+      <div className={classes.main}>
+        <div className={classes.marker}>
+          <div className={classes.box}></div>
+        </div>
+      </div>
       <div className={classes.rest}></div>
     </div>
   );
