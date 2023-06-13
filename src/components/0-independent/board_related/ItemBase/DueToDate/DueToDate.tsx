c11cf052b3bdb6f21539dfcb391ff6563c814caa
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 22:38:12 +0200

    added due to date

--- src/components/0-independent/board_related/ItemBase/DueToDate/DueToDate.tsx
+++ src/components/0-independent/board_related/ItemBase/DueToDate/DueToDate.tsx
@@ -3,7 +3,12 @@ import classes from "./_DueToDate.module.scss";
 
 //---------COMPONENT----------\
 const DueToDate = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>Mo, 28.07.2023</div>
+      {/* <input type="date" /> */}
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
