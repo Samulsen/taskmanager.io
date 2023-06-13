667455d6f6a4da193ad15bb82e5a70db1ee0fff8
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 23:08:18 +0200

    comment base struc done

--- src/components/0-independent/board_related/ItemBase/Comment/Comment.tsx
+++ src/components/0-independent/board_related/ItemBase/Comment/Comment.tsx
@@ -3,7 +3,14 @@ import classes from "./_Comment.module.scss";
 
 //---------COMPONENT----------\
 const Comment = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>
+        Lorem ipsum dolor sit amet, consemus munur...
+      </div>
+      {/* <textarea name="" id="" cols="30" rows="10"></textarea> */}
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
