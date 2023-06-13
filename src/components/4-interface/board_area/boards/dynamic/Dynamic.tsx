fd1a421a2c6f52b18f2ecca96116230daa38678d
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 23:36:14 +0200

    added base struc for board also to dynamic routes

--- src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
+++ src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
@@ -1,21 +1,37 @@
 //---------IMPORTS------------\
 
-import classes from "./_Dynamic.module.scss";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import classes from "./_Dynamic.module.scss";
+//__i-context________
 import { BoardContext } from "../../../../../context/BoardContext";
+//__i-components_____
+import HeadBase from "../../../../0-independent/board_related/HeadBase/HeadBase";
+import ItemBase from "../../../../0-independent/board_related/ItemBase/ItemBase";
+import FootBase from "../../../../0-independent/board_related/FootBase/FootBase";
 
 //---------COMPONENT----------\
 
 const Dynamic = function () {
+  //__c-hooks________
   const { boardID } = useParams();
   const { boardControl } = BoardContext()!;
+  //__c-logic________
 
+  //__c-effects______
   useEffect(() => {
     boardControl.setState(boardID as string);
   }, []);
-
-  return <div className={classes.body}></div>;
+  //__c-structure____
+  return (
+    <div className={classes.body}>
+      <HeadBase type="dynamic" />
+      <ItemBase type="dynamic" />
+      <ItemBase type="dynamic" />
+      <ItemBase type="dynamic" />
+      <FootBase type="dynamic" />
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
