3529622a13a44d77c8e0ba53cbc80dfcc3016acd
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 22:32:29 +0200

    added board origin colum

--- src/components/0-independent/board_related/ItemBase/BoardOrigin/BoardOrigin.tsx
+++ src/components/0-independent/board_related/ItemBase/BoardOrigin/BoardOrigin.tsx
@@ -3,7 +3,11 @@ import classes from "./_BoardOrigin.module.scss";
 
 //---------COMPONENT----------\
 const BoardOrigin = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>New Name</div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
