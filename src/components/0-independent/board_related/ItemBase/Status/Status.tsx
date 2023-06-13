ea7dfdd99401d7aa731eda540231a28021cf2e5b
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 22:42:31 +0200

    added base for status

--- src/components/0-independent/board_related/ItemBase/Status/Status.tsx
+++ src/components/0-independent/board_related/ItemBase/Status/Status.tsx
@@ -3,7 +3,11 @@ import classes from "./_Status.module.scss";
 
 //---------COMPONENT----------\
 const Status = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>In Progress</div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
