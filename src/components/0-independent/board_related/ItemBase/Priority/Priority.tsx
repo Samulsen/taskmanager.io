0f0df3b1e6e057137ade13927443330cbbba9678
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 23:03:13 +0200

    base for prio added

--- src/components/0-independent/board_related/ItemBase/Priority/Priority.tsx
+++ src/components/0-independent/board_related/ItemBase/Priority/Priority.tsx
@@ -3,7 +3,12 @@ import classes from "./_Priority.module.scss";
 
 //---------COMPONENT----------\
 const Priority = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>1</div>
+      {/* <input type="number" name="" id="" /> */}
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
