30eed917c97e0a581eb6198a64198e4071958e24
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 22:21:04 +0200

    added Task base, + mixin for body

--- src/components/0-independent/board_related/ItemBase/ItemBase.tsx
+++ src/components/0-independent/board_related/ItemBase/ItemBase.tsx
@@ -3,8 +3,35 @@
 import { FC } from "react";
 import classes from "./_ItemBase.module.scss";
 
+//__i-components_____
+
+import Status from "./Status/Status";
+import Priority from "./Priority/Priority";
+import DueToDate from "./DueToDate/DueToDate";
+import Task from "./Task/Task";
+import Comment from "./Comment/Comment";
+import BoardOrigin from "./BoardOrigin/BoardOrigin";
+
 //---------COMPONENT----------\
-const ItemBase: FC<{ type: string }> = function () {
+
+const ItemBase: FC<{ type: string }> = function ({ type }) {
+  //__c-hooks________
+
+  //__c-logic________
+
+  const Logic = {
+    UI: {
+      evaluateType() {
+        if (type === "total") {
+          return <BoardOrigin />;
+        } else {
+          return <></>;
+        }
+      },
+    },
+  };
+
+  //__c-structure____
   return (
     <div className={classes.body}>
       <div className={classes.grouplineMid}></div>
@@ -12,6 +39,12 @@ const ItemBase: FC<{ type: string }> = function () {
         <div className={classes.marker}>
           <div className={classes.box}></div>
         </div>
+        <Task />
+        {Logic.UI.evaluateType()}
+        <DueToDate />
+        <Status />
+        <Priority />
+        <Comment />
       </div>
       <div className={classes.rest}></div>
     </div>
--- src/components/0-independent/board_related/ItemBase/Task/Task.tsx
+++ src/components/0-independent/board_related/ItemBase/Task/Task.tsx
@@ -2,8 +2,13 @@
 import classes from "./_Task.module.scss";
 
 //---------COMPONENT----------\
+
 const Task = function () {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.display}>This is a Task</div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
