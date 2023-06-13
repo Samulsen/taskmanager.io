fc3ab3d6611cdfdb2fc13971c5a31caf5896442c
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 20:03:38 +0200

    fixed footer bug with grey border

--- src/components/0-independent/board_related/FootBase/FootBase.tsx
+++ src/components/0-independent/board_related/FootBase/FootBase.tsx
@@ -5,11 +5,27 @@ import classes from "./_FootBase.module.scss";
 
 //---------COMPONENT----------\
 const FootBase: FC<{ type: string }> = function ({ type }) {
+  const Logic = {
+    UI: {
+      evaluateType() {
+        if (type === "total") {
+          return (
+            <div className={classes.forTotal}>
+              Add task in specific board...
+            </div>
+          );
+        } else {
+        }
+      },
+    },
+  };
+
   return (
     <div className={classes.body}>
       <div className={classes.grouplineLower}></div>
       <div className={classes.main}>
         <div className={classes.quad}></div>
+        {Logic.UI.evaluateType()}
       </div>
       <div className={classes.rest}></div>
     </div>
