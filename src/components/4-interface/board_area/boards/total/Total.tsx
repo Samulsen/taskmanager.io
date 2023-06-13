011e6c818f5363caa6e83c0dd1f3103cc7fe2926
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-13 12:19:09 +0200

    css struc for total base

--- src/components/4-interface/board_area/boards/total/Total.tsx
+++ src/components/4-interface/board_area/boards/total/Total.tsx
@@ -5,6 +5,11 @@ import classes from "./_Total.module.scss";
 //---------COMPONENT----------\
 
 const Total = function () {
+  //__c-hooks________
+
+  //__c-logic________
+
+  //__c-structure____
   return <div className={classes.body}></div>;
 };
 
