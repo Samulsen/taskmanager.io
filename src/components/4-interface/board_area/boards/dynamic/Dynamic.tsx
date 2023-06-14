8941adf43d89b08ad9590242c0da264ac7a81e54
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-14 15:27:51 +0200

    added hook for reading collection data of items

--- src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
+++ src/components/4-interface/board_area/boards/dynamic/Dynamic.tsx
@@ -1,14 +1,18 @@
 //---------IMPORTS------------\
 
+import { onSnapshot, collection, query } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import classes from "./_Dynamic.module.scss";
+
 //__i-context________
 import { BoardContext } from "../../../../../context/BoardContext";
 //__i-components_____
 import HeadBase from "../../../../0-independent/board_related/HeadBase/HeadBase";
 import ItemBase from "../../../../0-independent/board_related/ItemBase/ItemBase";
 import FootBase from "../../../../0-independent/board_related/FootBase/FootBase";
+import { db } from "../../../../../firebase";
+import { AuthContext } from "../../../../../context/AuthContext";
 
 //---------COMPONENT----------\
 
@@ -17,6 +21,7 @@ const Dynamic = function () {
 
   const { boardID } = useParams();
   const { boardControl } = BoardContext()!;
+  const uid = AuthContext()?.userObject?.uid;
 
   //__c-logic________
 
@@ -26,6 +31,21 @@ const Dynamic = function () {
 
   useEffect(() => {
     boardControl.setState(boardID as string);
+  }, []);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, `MainUserDataPool_${uid}`, "UserBoards", `${boardID}`)
+    );
+    const unsub = onSnapshot(q, (itemsSnapshot) => {
+      if (!itemsSnapshot.metadata.hasPendingWrites) {
+        itemsSnapshot.forEach((item) => {
+          console.log(item.id, item.data());
+        });
+      }
+    });
+
+    return unsub;
   }, []);
 
   //__c-structure____
