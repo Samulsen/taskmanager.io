e9fb2e674e205ebfa138bd665ea3c0ae37e75cde
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-14 12:49:52 +0200

    added itemdata type and add task base

--- src/components/0-independent/board_related/FootBase/FootBase.tsx
+++ src/components/0-independent/board_related/FootBase/FootBase.tsx
@@ -1,12 +1,37 @@
 //---------IMPORTS------------\
 
-import { FC } from "react";
+import { FC, useRef } from "react";
 import classes from "./_FootBase.module.scss";
+import { itemdata } from "../../../../types/types";
+import { serverTimestamp } from "firebase/firestore";
+import { useParams } from "react-router-dom";
 
 //---------COMPONENT----------\
 const FootBase: FC<{ type: string }> = function ({ type }) {
+  //__c-hooks________
+
+  const newItemInputRef = useRef<HTMLInputElement>(null);
+  const { boardID } = useParams();
+
+  //__c-logic________
   const Logic = {
+    Data: {
+      createItem() {
+        const nameValue = newItemInputRef.current!.value;
+        const emptyData: itemdata = {
+          type: "item",
+          timestamp: serverTimestamp(),
+          taskname: nameValue,
+          board_origin: boardID,
+          due_to_date: "none",
+          status: "none",
+          priority: 0,
+          comment: "",
+        };
+      },
+    },
     UI: {
+      evaluateSubmissionValidity() {},
       evaluateType() {
         if (type === "total") {
           return (
@@ -15,11 +40,20 @@ const FootBase: FC<{ type: string }> = function ({ type }) {
             </div>
           );
         } else {
+          return (
+            <input
+              ref={newItemInputRef}
+              className={classes.addTask}
+              type="text"
+              placeholder="+ Add a Task"
+            />
+          );
         }
       },
     },
   };
 
+  //__c-structure____
   return (
     <div className={classes.body}>
       <div className={classes.grouplineLower}></div>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,24 @@
+//---------IMPORTS------------\
+
+import { FieldValue } from "firebase/firestore";
 import { Dispatch, SetStateAction } from "react";
 
+//---------EXPORTS------------\
+
+//__NOTE: Registerform
 export type input = boolean | string;
 export type nameValidator = Dispatch<SetStateAction<input>>;
-// ofset head
+
+//__NOTE: Board Data
+
+export interface itemdata {
+  type: string;
+  //   created_at_timestamp: { seconds: number; nanoseconds: number };
+  timestamp: any;
+  board_origin: string | undefined;
+  due_to_date: number | string;
+  taskname: string;
+  status: string;
+  priority: number;
+  comment: string;
+}
