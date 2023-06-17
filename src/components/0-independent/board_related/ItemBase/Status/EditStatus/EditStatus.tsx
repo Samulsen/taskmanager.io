6b9b05c507a1823688167fab013e767598e892de
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-17 10:51:06 +0200

    edit status done

--- src/components/0-independent/board_related/ItemBase/Status/EditStatus/EditStatus.tsx
+++ src/components/0-independent/board_related/ItemBase/Status/EditStatus/EditStatus.tsx
@@ -4,6 +4,9 @@ import classes from "./_EditStatus.module.scss";
 import { FC, Dispatch, SetStateAction, MouseEvent } from "react";
 import useClickOutside from "../../../../../../hooks/useClickOutside";
 import { itemOrigin } from "../../ItemBase";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../../../../firebase";
+import { AuthContext } from "../../../../../../context/AuthContext";
 
 //----------PRE---------------\
 
@@ -23,6 +26,8 @@ const EditStatus: FC<props> = function ({
 }) {
   //__c-hooks________
 
+  const uid = AuthContext()!.userObject!.uid;
+
   //__c-logic________
 
   const Logic = {
@@ -36,16 +41,36 @@ const EditStatus: FC<props> = function ({
     Data: {
       applyLabel(event: MouseEvent<HTMLDivElement>) {
         const targetOrigin = (event.target as any).innerHTML;
+        const itemDocRef = doc(
+          db,
+          `MainUserDataPool_${uid}`,
+          "UserBoards",
+          itemOrigin.board,
+          itemOrigin.id
+        );
         if (targetOrigin === "") {
-          //none
+          const updatedData = {
+            status: "none",
+          };
+          updateDoc(itemDocRef, updatedData).then(Logic.UI.Edit.disable);
         }
-        if (targetOrigin === "done") {
+        if (targetOrigin === "Done!") {
+          const updatedData = {
+            status: "done",
+          };
+          updateDoc(itemDocRef, updatedData).then(Logic.UI.Edit.disable);
         }
-
-        if (targetOrigin === "progress") {
+        if (targetOrigin === "In Progress!") {
+          const updatedData = {
+            status: "progress",
+          };
+          updateDoc(itemDocRef, updatedData).then(Logic.UI.Edit.disable);
         }
-
-        if (targetOrigin === "untouched") {
+        if (targetOrigin === "Untouched!") {
+          const updatedData = {
+            status: "untouched",
+          };
+          updateDoc(itemDocRef, updatedData).then(Logic.UI.Edit.disable);
         }
       },
     },
