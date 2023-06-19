c4129d054c31bf27c1dcb07dbb12f1ed338615ee
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-20 00:22:11 +0200

    added select all to head

--- src/components/0-independent/board_related/HeadBase/TickboxAll/TickboxAll.tsx
+++ src/components/0-independent/board_related/HeadBase/TickboxAll/TickboxAll.tsx
@@ -1,10 +1,13 @@
 //---------IMPORTS------------\
 
 //__i-libraries______
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 //__i-style__________
 import classes from "./_TickboxAll.module.scss";
 //__i-context________
+import { ItemControlContext } from "../../../../../context/ItemControlContext";
+import { ActiveDataContext } from "../../../../../context/ActiveDataContext";
+import { itemOrigin } from "../../ItemBase/ItemBase";
 //__i-components_____
 
 //----------PRE---------------\
@@ -15,21 +18,70 @@ interface props {}
 
 //---------COMPONENT----------\
 
-const TickboxAll:FC<props> = function () {
+const TickboxAll: FC<props> = function () {
   //__c-hooks________
+
+  const { closingMode, itemControl, itemSelection } = ItemControlContext()!;
+  const { clientAffectedData } = ActiveDataContext()!;
+  const [selectAllState, setSelectAllState] = useState(false);
 
   //__c-logic________
 
   const Logic = {
-    UI: {},
+    UI: {
+      handleClick() {
+        if (selectAllState) {
+          this.disable();
+        } else {
+          this.enable();
+        }
+      },
+      enable() {
+        const allCurrentIdsArr: itemOrigin[] = clientAffectedData.map(
+          (item) => {
+            return { id: item.id, board: item.board_origin };
+          }
+        );
+        itemControl.setState(true);
+        itemSelection.update(allCurrentIdsArr);
+      },
+      disable() {
+        itemSelection.update([]);
+        closingMode.setState(true);
+      },
+
+      Classes: {
+        forBox() {
+          return selectAllState
+            ? `${classes.box} ${classes.selected}`
+            : `${classes.box} ${classes.unselected}`;
+        },
+      },
+    },
     Data: {},
   };
 
+  //__c-effects______
+
+  useEffect(() => {
+    if (itemSelection.list.length === clientAffectedData.length) {
+      setSelectAllState(true);
+    } else {
+      setSelectAllState(false);
+    }
+  }, [itemSelection.list]);
+
   //__c-structure____
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <div
+        className={Logic.UI.Classes.forBox()}
+        onClick={Logic.UI.handleClick.bind(Logic.UI)}
+      ></div>
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
 
 export default TickboxAll;
-
