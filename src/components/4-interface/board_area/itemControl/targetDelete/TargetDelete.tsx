9cafb4916ca0c71b78d7f37d00a9c92cbedc3af3
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-19 14:26:06 +0200

    added delete item base

--- src/components/4-interface/board_area/itemControl/targetDelete/TargetDelete.tsx
+++ src/components/4-interface/board_area/itemControl/targetDelete/TargetDelete.tsx
@@ -4,17 +4,18 @@
 import { FC } from "react";
 //__i-style__________
 import classes from "./_TargetDelete.module.scss";
+import deleteIcon from "./delete.svg";
 //__i-context________
 //__i-components_____
 
-//----------PRE---------------
+//----------PRE---------------\
 //__p-types_________
 
 interface props {}
 
 //---------COMPONENT----------\
 
-const TargetDelete:FC<props> = function () {
+const TargetDelete: FC<props> = function () {
   //__c-hooks________
 
   //__c-logic________
@@ -25,10 +26,14 @@ const TargetDelete:FC<props> = function () {
   };
 
   //__c-structure____
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <img src={deleteIcon} alt="deleteIconItemControl" />
+      Delete
+    </div>
+  );
 };
 
 //---------EXPORTS------------\
 
 export default TargetDelete;
-
