28d93a1b43edf0fce89bce731c0ca532d20ba84d
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-19 22:43:28 +0200

    basic selection message added

--- src/components/4-interface/board_area/itemControl/selectionMessage/SelectionMessage.tsx
+++ src/components/4-interface/board_area/itemControl/selectionMessage/SelectionMessage.tsx
@@ -4,6 +4,7 @@
 import { FC } from "react";
 //__i-style__________
 import classes from "./_SelectionMessage.module.scss";
+import { ItemControlContext } from "../../../../../context/ItemControlContext";
 //__i-context________
 //__i-components_____
 
@@ -17,15 +18,25 @@ interface props {}
 const SelectionMessage: FC<props> = function () {
   //__c-hooks________
 
+  const { itemSelection } = ItemControlContext()!;
+
   //__c-logic________
 
   const Logic = {
-    UI: {},
+    UI: {
+      decidePlural() {
+        if (itemSelection.list.length === 1) {
+          return "Task selected";
+        } else {
+          return "Tasks selected";
+        }
+      },
+    },
     Data: {},
   };
 
   //__c-structure____
-  return <div className={classes.body}>Tasks selected</div>;
+  return <div className={classes.body}>{Logic.UI.decidePlural()}</div>;
 };
 
 //---------EXPORTS------------\
