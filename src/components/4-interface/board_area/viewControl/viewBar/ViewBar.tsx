4fb9dd7999afafab757af0d43c5fc8e3439f9925
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-21 00:31:39 +0200

    default sort mode

--- src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
+++ src/components/4-interface/board_area/viewControl/viewBar/ViewBar.tsx
@@ -19,12 +19,12 @@ import { BoardContext } from "../../../../../context/BoardContext";
 
 //__i-components_____
 
+import { CompositItemData } from "../../../../../types/types";
 import ViewTemplate from "./viewTemplate/ViewTemplate";
 import SepBig from "./seperator/sepBig/SepBig";
 import EffectFilter from "./effectFilter/EffectFilter";
 import EffectSort from "./effectSort/EffectSort";
 import SepSmall from "./seperator/sepSmall/SepSmall";
-import { CompositItemData } from "../../../../../types/types";
 
 //---------COMPONENT----------\
 
@@ -38,118 +38,51 @@ const ViewBar = function () {
   //__c-logic________
 
   const Logic = {
-    initAffectionChain() {
-      return Promise.resolve(rawQueryItems.data);
-    },
+    tempAffectionDataPool: rawQueryItems.data as CompositItemData[],
     View: {
-      Option: {
-        forHome(
-          none_AffectedData: CompositItemData[]
-        ): Promise<CompositItemData[]> {
-          return new Promise((resolve) => {
-            resolve(none_AffectedData);
-          });
-        },
-        forDone(
-          none_AffectedData: CompositItemData[]
-        ): Promise<CompositItemData[]> {
-          return new Promise((resolve) => {
-            const onlyDone = none_AffectedData.filter(
-              (item) => item.status === "done"
-            );
-            resolve(onlyDone);
-          });
-        },
-        // forProgress(none_AffectedData: CompositItemData[]) {},
-        // forUntouched(none_AffectedData: CompositItemData[]) {},
-      },
-      decide(none_AffectedData: CompositItemData[]) {
-        if (viewControl.state === "Home") {
-          return this.Option.forHome(none_AffectedData);
-        }
-        if (viewControl.state === "Done") {
-          return this.Option.forDone(none_AffectedData);
-        }
-        // if (viewControl.state === "In Progress") {
-        //   return this.Option.forProgress(none_AffectedData);
-        // }
-        // if (viewControl.state === "Untouched") {
-        //   return this.Option.forUntouched(none_AffectedData);
-        // }
-        return [];
+      decide() {
+        return Logic;
       },
     },
     Filter: {
-      Option: {
-        unaffected(
-          view_AffectedData: CompositItemData[]
-        ): Promise<CompositItemData[]> {
-          return new Promise((resolve) => {
-            resolve(view_AffectedData);
-          });
-        },
-      },
-      decide(view_AffectedData: CompositItemData[]) {
-        if (filterControl.state === "none") {
-          return this.Option.unaffected(view_AffectedData);
-        }
-        return [];
+      decide() {
+        return Logic;
       },
     },
     Sort: {
-      Option: {
-        unaffected(
-          view_filter_AffectedData: CompositItemData[]
-        ): Promise<CompositItemData[]> {
-          return new Promise((resolve) => {
-            if (sortControl.direction === "ase") {
-              const ascendingOrder = view_filter_AffectedData.sort(
-                (a, b) => a.timestamp.seconds - b.timestamp.seconds
-              );
+      Options: {
+        unaffected() {
+          if (sortControl.direction === "ase") {
+            const ascendingOrder = Logic.tempAffectionDataPool.sort(
+              (a, b) => a.timestamp.seconds - b.timestamp.seconds
+            );
 
-              resolve(ascendingOrder);
-            } else {
-              const descendingOrder = view_filter_AffectedData.sort(
-                (a, b) => b.timestamp.seconds - a.timestamp.seconds
-              );
+            Logic.tempAffectionDataPool = ascendingOrder;
+          } else {
+            const descendingOrder = Logic.tempAffectionDataPool.sort(
+              (a, b) => b.timestamp.seconds - a.timestamp.seconds
+            );
 
-              resolve(descendingOrder);
-            }
-          });
+            Logic.tempAffectionDataPool = descendingOrder;
+          }
         },
       },
-      decide(view_filter_AffectedData: CompositItemData[]) {
+      decide() {
         if (sortControl.state === "creationtime") {
-          return this.Option.unaffected(view_filter_AffectedData);
+          Logic.Sort.Options.unaffected();
         }
-        if (sortControl.state === "priority") {
-        }
-        if (sortControl.state === "date") {
-        }
-        return [];
+
+        return Logic;
       },
     },
-    finishAffectionChain(view_filter_sort_affectedData: CompositItemData[]) {
-      setClienAffectedData(view_filter_sort_affectedData);
+    setFinalAffection() {
+      setClienAffectedData(this.tempAffectionDataPool);
     },
   };
 
   //__c-effects______
 
-  setClienAffectedData(rawQueryItems.data);
-
-  // useEffect(() => {
-  // Logic.initAffectionChain()
-  //   .then(Logic.View.decide.bind(Logic.View))
-  //   .then(Logic.Filter.decide.bind(Logic.Filter))
-  //   .then(Logic.Sort.decide.bind(Logic.Sort))
-  //   .then(Logic.finishAffectionChain.bind(Logic));
-  // }, [
-  //   viewControl.state,
-  //   sortControl.state,
-  //   filterControl.state,
-  //   rawQueryItems.data,
-  // ]);
+  Logic.View.decide().Filter.decide().Sort.decide().setFinalAffection();
 
   //__c-structure____
 
