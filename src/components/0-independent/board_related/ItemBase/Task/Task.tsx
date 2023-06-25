97560ef941b0114cb995bd8af00b1eb07d767bde
Samuil Berenzon <samus.dev.space@gmail.com>
2023-06-25 22:22:14 +0200

    refac of taskname with fit text

--- src/components/0-independent/board_related/ItemBase/Task/Task.tsx
+++ src/components/0-independent/board_related/ItemBase/Task/Task.tsx
@@ -9,14 +9,14 @@ import {
   KeyboardEvent,
   useEffect,
 } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../../../firebase";
 //__i-style__________
 import classes from "./_Task.module.scss";
 //__i-context________
 import { AuthContext } from "../../../../../context/AuthContext";
 //__i-helper_________
 import { itemOrigin } from "../ItemBase";
-import { db } from "../../../../../firebase";
-import { doc, updateDoc } from "firebase/firestore";
 
 //----------PRE---------------\
 
@@ -30,6 +30,7 @@ interface props {
 const Task: FC<props> = function ({ displayValue, itemOrigin }) {
   //__c-hooks________
 
+  const [defaultValue, updateDefaultValue] = useState("");
   const [requestValidity, setRequestValidity] = useState(true);
   const inputRef = useRef<HTMLInputElement>(null);
   const uid = AuthContext()!.userObject!.uid;
@@ -38,37 +39,44 @@ const Task: FC<props> = function ({ displayValue, itemOrigin }) {
 
   const Logic = {
     Data: {
-      updateName() {
+      checkRequest() {
         const editedDisplayValue = inputRef.current!.value;
         if (editedDisplayValue.trim().length === 0) {
           setRequestValidity(false);
+          return false;
         } else {
-          const itemDocRef = doc(
-            db,
-            `MainUserDataPool_${uid}`,
-            "UserBoards",
-            itemOrigin.board,
-            itemOrigin.id
-          );
-          const updatedData = {
-            taskname: editedDisplayValue,
-          };
-          Logic.UI.unfocus();
-          updateDoc(itemDocRef, updatedData);
+          return true;
         }
+      },
+      updateName(): Promise<void> {
+        const editedDisplayValue = inputRef.current!.value;
+        const itemDocRef = doc(
+          db,
+          `MainUserDataPool_${uid}`,
+          "UserBoards",
+          itemOrigin.board,
+          itemOrigin.id
+        );
+        const updatedData = {
+          taskname: editedDisplayValue,
+        };
+        Logic.UI.unfocus();
+        return updateDoc(itemDocRef, updatedData);
       },
       handleKeydown(event: KeyboardEvent) {
         if (event.target === event.currentTarget) {
           if (event.key === "Enter") {
-            Logic.Data.updateName();
-            return;
+            if (this.checkRequest()) {
+              Logic.Data.updateName().then(Logic.UI.fitText);
+            }
           }
         }
       },
       handleUnfocus(event: FocusEvent) {
         if (event.target === event.currentTarget) {
-          Logic.Data.updateName();
-          return;
+          if (this.checkRequest()) {
+            Logic.Data.updateName().then(Logic.UI.fitText);
+          }
         }
       },
     },
@@ -81,12 +89,15 @@ const Task: FC<props> = function ({ displayValue, itemOrigin }) {
           ? { placeholder: "" }
           : { placeholder: "cannot be empty!" };
       },
+      showAllText() {
+        updateDefaultValue(displayValue);
+      },
       fitText() {
-        // if (displayValue.length > 28) {
-        //   updateDefaultValue(displayValue.slice(0, 28) + "...");
-        // } else {
-        //   updateDefaultValue(displayValue);
-        // }
+        if (displayValue.length > 28) {
+          updateDefaultValue(displayValue.slice(0, 28) + "...");
+        } else {
+          updateDefaultValue(displayValue);
+        }
       },
     },
   };
@@ -104,10 +115,11 @@ const Task: FC<props> = function ({ displayValue, itemOrigin }) {
       <input
         ref={inputRef}
         type="text"
-        defaultValue={displayValue}
+        defaultValue={defaultValue}
         name="editTaskInput"
-        onBlur={Logic.Data.handleUnfocus}
-        onKeyDown={Logic.Data.handleKeydown}
+        onFocus={Logic.UI.showAllText}
+        onBlur={Logic.Data.handleUnfocus.bind(Logic.Data)}
+        onKeyDown={Logic.Data.handleKeydown.bind(Logic.Data)}
         {...Logic.UI.evaluateSubmissionValidity()}
       />
     </div>
